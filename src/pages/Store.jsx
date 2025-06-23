606689c7c30f7cb48caea0cebd48bbe43479f5ac
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-06-23 21:40:33 +0100

    Create Store.jsx

---
+++ src/pages/Store.jsx
@@ -0,0 +1,8 @@
+export default function Store() {
+  return (
+    <main style={{ fontFamily: 'monospace', padding: '20px' }}>
+      <h1>🛍️ PokéMart</h1>
+      <p>Buy Pokéballs and other items here (coming soon!).</p>
+    </main>
+  );
+}
