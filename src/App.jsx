4e5b4ed65c485b05fbef7d862cf4b0a6b29c5ce6
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-06-23 21:20:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/home';
+import Lab from '../pages/lab';
+import Store from '../pages/store';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lab" element={<Lab />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
