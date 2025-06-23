380e6e37755e0c719b53c4c62f5724d0dc2c2755
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-06-23 21:12:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Lab from './pages/lab';
-import Store from './pages/store';
+import Home from '../pages/home';
+import Lab from '../pages/lab';
+import Store from '../pages/store';
 
 function App() {
   return (
     <Router>
-      <ThemeSwitcher />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/lab " element={<Lab />} />
+        <Route path="/lab" element={<Lab />} />
         <Route path="/store" element={<Store />} />
       </Routes>
     </Router>
