9c378e01ed1923558cd815c3599565dd01a5cd70
ray-seal <rayseal1986@gmail.com>
2025-06-23 12:13:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import data from '../public/pokedex.js';
 import dynamic from 'next/dynamic';
 const Battle = dynamic(() => import('../components/battle.js'), { ssr: false });
-
 export default function Home() {
   const [game, setGame] = useState(null);
   const [wild, setWild] = useState(null);
