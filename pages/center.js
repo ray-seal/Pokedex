7581c2b0f0f3503274d12534ada38e48ed79785c
ray-seal <rayseal1986@gmail.com>
2025-06-26 01:11:17 +0100

    Update center.js

--- pages/center.js
+++ pages/center.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import pokedex from '../public/pokedex.json';
+import pokedex from '../public/wildlifejournal.json';
 import { getPokemonStats } from '../lib/pokemonStats';
 
 export default function Center() {
