3c886bd21fa4b997879d821099e6bc4f3930dd36
ray-seal <rayseal1986@gmail.com>
2025-06-26 05:55:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 // pages/_app.js
-
-import '../styles/globals.css' // Optional: Only if you have a global stylesheet
 import { GameProvider } from '../context/GameContext'
 
 export default function MyApp({ Component, pageProps }) {
